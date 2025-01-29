a4471c4436d5e4eecf547370bc028e3f4596e26d
in2dm3-a <ayla.hernandezca@elorrieta-errekamari.com>
2025-01-29 13:09:30 +0100

    codigo crear reunion, sin terminar, revisar

--- app/src/main/java/com/example/frameworkeducativoreto2grupo2/CrearReunion/CrearReunion.java
+++ app/src/main/java/com/example/frameworkeducativoreto2grupo2/CrearReunion/CrearReunion.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +16,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.example.frameworkeducativoreto2grupo2.Clases.TipoUsuario;
+import com.example.frameworkeducativoreto2grupo2.Clases.UserAdapter;
 import com.example.frameworkeducativoreto2grupo2.Cliente.Cliente;
 import com.example.frameworkeducativoreto2grupo2.InterfazProfesor.DatosEstudiantes;
 import com.example.frameworkeducativoreto2grupo2.InterfazProfesor.MenuProfesor;
+import com.example.frameworkeducativoreto2grupo2.InterfazProfesor.PerfilProfesor;
 import com.example.frameworkeducativoreto2grupo2.R;
 
 import java.io.DataInputStream;
@@ -25,13 +30,19 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import Modelo.Users;
 
 public class CrearReunion extends AppCompatActivity {
     private DataInputStream dis;
     private DataOutputStream dos;
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
-    int IDUserLog;
+
+    Users user = new Users();
+    private List<Users> listaUsers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +55,6 @@ public class CrearReunion extends AppCompatActivity {
             return insets;
         });
 
-        //recoger el inten que ha comenzado este activity
-        Intent intent = getIntent();
-        //recoger los datos mandados con el intent
-        IDUserLog = intent.getIntExtra("IDUserLog", -1); //-1 --> valor por defecto si no encuentra el getExtra
 
         try {
             oos = Cliente.getInstance().getObjectOutputStream();
@@ -72,22 +79,93 @@ public class CrearReunion extends AppCompatActivity {
         EditText txtUbicacion = findViewById(R.id.editTextUbicacion);
         EditText txtAula = findViewById(R.id.editTextAula);
 
+        //setear el nombre del solicitante en el campo nombreuser*********************
+        txtNombreUser.setText(user.getNombre());
+
+        //setear en el spinner los profesores o alumnos, dependiendo del tipo de user
+        if (user.getTipo().equals(TipoUsuario.PROFESOR.getTipoUser())) { //si es un profesor --> lista de alumnos
+            //alumnos
+            new Thread(() -> {
+                try {
+                    //Opcion seleccionada 10 recoger alumnos
+                    dos.writeInt(10);
+                    dos.flush();
+
+                    //leer el List
+                    listaUsers = (List<Users>) ois.readObject();
+
+                    //actualizamos el spinner con alumnos***
 
 
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        } else { //si es un alumno --> lista de profesores
+            //profesores
+            new Thread(() -> {
+                try {
+                    //opcion seleccionada 11 obtener profesores
+                    dos.writeInt(11);
+                    dos.flush();
+
+                    //leer el List
+                    listaUsers = (List<Users>) ois.readObject();
+
+                    //actualizamos el spinner con profesores***
 
 
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
 
+        String userReunion = ""; //la persona con la que se quiere tener la reunion recogido del spinner
 
         //listener boton nueva reunion ------------------------------------------------------------------------------- BOTON CREAR NUEVA REUNION
         btnCrearNuevaReunion.setOnClickListener(view -> {
-            //*****agregar los datos a la db
+            //mandar los datos al servidor
+            new Thread(() -> {
+                try {
+                    //Opcion seleccionada - enviar datos para crear reunion
+                    dos.writeInt(9);
+                    dos.flush();
+
+                    //datos
+                    dos.writeUTF(String.valueOf(txtTitulo.getText()));
+                    dos.flush();
+
+                    dos.writeUTF(String.valueOf(txtTema.getText()));
+                    dos.flush();
+
+                    //calendario DATE**********************************
+
+                    dos.writeUTF((String) txtNombreUser.getText());
+                    dos.flush();
+
+                    dos.writeUTF(userReunion);
+                    dos.flush();
+
+                    dos.writeUTF(String.valueOf(txtUbicacion.getText()));
+                    dos.flush();
+
+                    dos.writeUTF(String.valueOf(txtAula.getText())); //QUE DATATYPE ES??************************
+                    dos.flush();
+
+                    runOnUiThread(() -> Toast.makeText(this, "rEUNION CREADA", Toast.LENGTH_SHORT).show());
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+
         });
 
 
         //listener boton atras ------------------------------------------------------------------------------- BOTON ATRAS
         btnAtras.setOnClickListener(view -> {
             Intent menuProfesor = new Intent(CrearReunion.this, MenuProfesor.class);
-            menuProfesor.putExtra("IDUserLog", IDUserLog);
             startActivity(menuProfesor);
         });
 
